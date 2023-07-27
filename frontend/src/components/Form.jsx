cda8eb7a3522d9dab55ef8ff4a98b29479129981
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-07-27 12:04:55 -0300

    Fix: React form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,30 +1,35 @@
+import { useState } from "react";
 import "../assets/Form.css";
 import "../index.css";
 
 export default function Form() {
+  const [newQuery, setNewQuery] = useState("");
+
+  // this is for displaying the search query of the user
+  const [queries, setQueries] = useState([]);
+
+  // function that will
+  function handleSearch(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <section>
-        <div className="container">
-          <div className="form-wrapper">
-            <div className="form-data-description">
-              <p>
-                Here you can search for scientific papers on different subjects
-              </p>
-            </div>
-
-            <form action="">
-              <div className="input-row">
-                <label htmlFor=""></label>
-                <input type="text" placeholder="Type something" />
-              </div>
-              <div className="input-row">
-                <input type="submit" value={"Search"} />
-              </div>
-            </form>
-          </div>
+      <form onSubmit={handleSearch} action="" className="form">
+        <div className="input-row">
+          <label htmlFor="query">Search for any scientific paper</label>
+          <input
+            type="text"
+            name="query"
+            className="input-field"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
-      </section>
+        <div className="input-row">
+          <input type="submit" value={"Search"} />
+        </div>
+      </form>
     </>
   );
 }
